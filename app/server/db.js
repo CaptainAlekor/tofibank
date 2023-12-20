53aabb6fce827bca48ae159f20b2496ed418466c
CaptainAlekor <79224183+CaptainAlekor@users.noreply.github.com>
2023-12-20 18:21:52 +0300

    Update db.js

--- app/server/db.js
+++ app/server/db.js
@@ -1,15 +1,41 @@
-const {Sequelize} = require('sequelize')
+const {Sequelize} = require('sequelize');
 
 module.exports = new Sequelize(
-    'ebanking',
-    'postgres',
-    'SpainPrince21',
     {
+        database: 'ebanking',
+        username: 'render_user',
+        password: 'i8IhyT3R0GjmqYy2VhVGTegYfztt5Vph',
         dialect: 'postgres',
-        host: 'localhost',
-        port: 5432
-    }
-)
+        host: 'dpg-cm1ga5en7f5s73e92v10-a.frankfurt-postgres.render.com', 
+        port: process.env.DB_PORT,
+        pool: {
+            max: 3,
+            min: 1,
+            idle: 10000,
+        },
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            },
+            keepAlive: true,
+        },
+        ssl:true,
+        
+    })
+
+// const {Sequelize} = require('sequelize')
+
+// module.exports = new Sequelize(
+//     'ebanking',
+//     'postgres',
+//     'SpainPrince21',
+//     {
+//         dialect: 'postgres',
+//         host: 'localhost',
+//         port: 5432
+//     }
+// )
 
 // const Pool = require('pg').Pool
 // const pool = new Pool({
