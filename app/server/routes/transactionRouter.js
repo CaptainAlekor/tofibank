6afd8669cf4b732d0a98f23f9d9b419c7bd98b30
CaptainAlekor <79224183+CaptainAlekor@users.noreply.github.com>
2023-12-20 17:47:29 +0300

    Delete app directory

--- app/server/routes/transactionRouter.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const transactionController = require('../controllers/transactionController')
-
-router.put('/transfer', transactionController.makeTransaction)
-
-module.exports = router
